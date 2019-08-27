bdcea5f133469cd73898f32260129e4d88f24b71
Charles Kettell <charleskettell@hotmail.co.uk>
2019-08-27 12:33:12 +0100

    deposit method adds to the transations

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,21 @@
 
 class BankAccount
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize(balance = 0)
     @balance = balance
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    date =
+    @transactions << {:balance => @balance, :credit => amount, :debit => 0, :date => Time.new}
+  end
+
+  def withdraw(amount)
+    @balance -= amount
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,9 +1,27 @@
 require 'bank_account'
+require 'timecop'
+
 
 describe BankAccount do
   it 'When a user deposits £100 to their account, the balance should be £100' do
     account = BankAccount.new
     account.deposit(100)
     expect(account.balance).to eq(100)
+  end
+
+  it "withdraws money from the user's account" do
+    account = BankAccount.new
+    account.deposit(100)
+    account.withdraw(50)
+    expect(account.balance).to eq(50)
+  end
+
+  it 'When a user makes a deposit, it records the date, amount and balance' do
+    Timecop.freeze(2019, 8, 27) do
+      date = Time.new
+      account = BankAccount.new
+      account.deposit(100)
+      expect(account.transactions.last).to eq({ :date => date, :credit => 100, :debit => 0, :balance => 100})
+    end
   end
 end
